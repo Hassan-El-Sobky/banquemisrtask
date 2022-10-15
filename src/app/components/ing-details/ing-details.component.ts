ab4e655dfdbe48408fe2c0c6bf06323fb67fd455
Hassan-El-Sobky <hassan_elsobky@hotmail.com>
2022-10-15 17:07:01 +0200

    feat/toggle

--- src/app/components/ing-details/ing-details.component.ts
+++ src/app/components/ing-details/ing-details.component.ts
@@ -3,25 +3,6 @@ import { Iingredints } from 'src/app/model/ingredints.model';
 import { IngredintsService } from 'src/app/state/ingredints.state';
 
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 @Component({
   selector: 'app-ing-details',
   templateUrl: './ing-details.component.html',
@@ -30,6 +11,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class IngDetailsComponent implements OnInit {
   displayedColumns: string[] = ['Quantity', 'Unit', 'Food', 'Calories','Weight'];
   dataSource=[];
+  toggleFlag:boolean=false;
   ingredintsState=this._ingSate.ingredintSubject.getValue();
   constructor(
     private _ingSate:IngredintsService
@@ -43,7 +25,10 @@ export class IngDetailsComponent implements OnInit {
        console.log(this.dataSource);
        
     });
-    
+   
   }
 
+  toggleFacts(){
+      this.toggleFlag=!this.toggleFlag;
+  }
 }
